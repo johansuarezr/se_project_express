8a8e90bbe29791f91de9669655529e9b6dcc001a
johansuarezr <johansuarez@Johans-MacBook-Air.local>
2025-03-04 21:55:24 -0500

    new changes

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
-const { JWT_SECRET = "super-strong-secret" } = process.env;
+const {
+  JWT_SECRET = "b290371a520f21bf81cb525fcf5d433290a74c0691481f676f9f101423d4b9f5",
+} = process.env;
 
 module.exports = { JWT_SECRET };
