5d6c19b56cb204fb373241a1ff27f686eb2c410b
johansuarezr <johansuarez@Johans-MacBook-Air.local>
2025-03-08 00:47:51 -0500

    braces fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { NotFoundError } = require("../customErrors/not-found-error");
+const NotFoundError = require("../customErrors/not-found-error");
 const userRouter = require("./users");
 const clothingRouter = require("./clothingItems");
 const { login, createUser } = require("../controllers/users");
